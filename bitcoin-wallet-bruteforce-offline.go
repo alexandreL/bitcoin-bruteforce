5e2a415db212268affc3db179db8a7272865d58a
Alexandre loubeyres <alexandre.loubeyres@gmail.com>
2024-12-30 00:41:51 +0100

    init

--- bitcoin-wallet-bruteforce-offline.go
+++ bitcoin-wallet-bruteforce-offline.go
@@ -3,149 +3,128 @@
 package main
 
 import (
-        "bufio"
-	"crypto/ecdsa"
-	"crypto/elliptic"
-	"crypto/rand"
+	"bufio"
 	"encoding/hex"
 	"fmt"
+	"github.com/btcsuite/btcd/btcec/v2"
+	"github.com/btcsuite/btcd/btcutil/base58"
 	"log"
 	"os"
 	"strconv"
 	"sync"
 
-	"github.com/btcsuite/btcutil/base58"
-	"golang.org/x/crypto/ripemd160"
 	"crypto/sha256"
+	"golang.org/x/crypto/ripemd160"
 )
 
 func readAddresses(filePath string) (map[string]bool, error) {
-    addresses := make(map[string]bool)
+	addresses := make(map[string]bool)
 
-    file, err := os.Open(filePath)
-    if err != nil {
-        return nil, err
-    }
-    defer file.Close()
+	file, err := os.Open(filePath)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
 
-    scanner := bufio.NewScanner(file)
-    for scanner.Scan() {
-        addresses[scanner.Text()] = true
-    }
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		addresses[scanner.Text()] = true
+	}
 
-    if err := scanner.Err(); err != nil {
-        return nil, err
-    }
+	if err := scanner.Err(); err != nil {
+		return nil, err
+	}
 
-    return addresses, nil
+	return addresses, nil
 }
 
 func generateKeyAndAddress() (string, string, error) {
-	privateKey, err := ecdsa.GenerateKey(elliptic.P256(), rand.Reader)
+	privateKey, err := btcec.NewPrivateKey()
+
 	if err != nil {
 		return "", "", err
 	}
 
-	publicKey := privateKey.PublicKey
-	address, err := publicKeyToAddress(publicKey)
+	serializedPublicKey := privateKey.PubKey().SerializeCompressed()
+	address, err := publicKeyToAddress(serializedPublicKey)
+
 	if err != nil {
 		return "", "", err
 	}
-
-	return hex.EncodeToString(privateKey.D.Bytes()), address, nil
+	return hex.EncodeToString(privateKey.Serialize()), address, nil
 }
 
-func publicKeyToAddress(publicKey ecdsa.PublicKey) (string, error) {
-	pubKeyBytes := append(publicKey.X.Bytes(), publicKey.Y.Bytes()...)
+func publicKeyToAddress(serializedPublicKey []byte) (string, error) {
+	publicSHA256 := sha256.Sum256(serializedPublicKey)
 
-	sha256Hash := sha256.New()
-	sha256Hash.Write(pubKeyBytes)
-	sha256Result := sha256Hash.Sum(nil)
+	ripemd160Hasher := ripemd160.New()
+	_, err := ripemd160Hasher.Write(publicSHA256[:])
+	if err != nil {
+		return "", err
+	}
 
-	ripemd160Hash := ripemd160.New()
-	ripemd160Hash.Write(sha256Result)
-	ripemd160Result := ripemd160Hash.Sum(nil)
+	publicRIPEMD160 := ripemd160Hasher.Sum(nil)
 
-	networkVersion := byte(0x00)
-	addressBytes := append([]byte{networkVersion}, ripemd160Result...)
-	checksum := sha256Checksum(addressBytes)
-	fullAddress := append(addressBytes, checksum...)
-
-	return base58.Encode(fullAddress), nil
-}
-
-func sha256Checksum(input []byte) []byte {
-	firstSHA := sha256.New()
-	firstSHA.Write(input)
-	result := firstSHA.Sum(nil)
-
-	secondSHA := sha256.New()
-	secondSHA.Write(result)
-	finalResult := secondSHA.Sum(nil)
-
-	return finalResult[:4]
+	return base58.CheckEncode(publicRIPEMD160[:], 0x00), nil
 }
 
 func worker(id int, wg *sync.WaitGroup, mutex *sync.Mutex, outputFile string, btcAddresses map[string]bool) {
-    defer wg.Done()
+	defer wg.Done()
 
-    for {
-        privateKey, publicAddress, err := generateKeyAndAddress()
-        if err != nil {
-            log.Printf("Worker %d: Failed to generate key and address: %s", id, err)
-            continue
-        }
+	for {
+		privateKey, publicAddress, err := generateKeyAndAddress()
+		if err != nil {
+			log.Printf("Worker %d: Failed to generate key and address: %s", id, err)
+			continue
+		}
 
-        matchFound := "No"
-        if _, exists := btcAddresses[publicAddress]; exists {
-            matchFound = "Yes"
-            fmt.Printf("Match Found! Privatekey: %s Publicaddress: %s\n", privateKey, publicAddress)
+		if _, exists := btcAddresses[publicAddress]; exists {
+			fmt.Printf("Match Found! Privatekey: %s Publicaddress: %s\n", privateKey, publicAddress)
 
-            mutex.Lock()
-            file, err := os.OpenFile(outputFile, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
-            if err != nil {
-                log.Printf("Worker %d: Failed to open file: %s", id, err)
-                mutex.Unlock()
-                continue
-            }
+			mutex.Lock()
+			file, err := os.OpenFile(outputFile, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+			if err != nil {
+				log.Printf("Worker %d: Failed to open file: %s", id, err)
+				mutex.Unlock()
+				continue
+			}
 
-            if _, err := file.WriteString(fmt.Sprintf("%s:%s\n", privateKey, publicAddress)); err != nil {
-                log.Printf("Worker %d: Failed to write to file: %s", id, err)
-            }
-            file.Close()
-            mutex.Unlock()
-        }
+			if _, err := file.WriteString(fmt.Sprintf("%s:%s\n", privateKey, publicAddress)); err != nil {
+				log.Printf("Worker %d: Failed to write to file: %s", id, err)
+			}
+			file.Close()
+			mutex.Unlock()
+		}
 
-        fmt.Printf("Private Key: %s Public Address: %s Match: %s\n", privateKey, publicAddress, matchFound)
-    }
+	}
 }
 
 func main() {
-    if len(os.Args) != 4 {
-        fmt.Println("Usage: ./golangscript <threads> <output-file.txt> <btc-address-file.txt>")
-        os.Exit(1)
-    }
+	if len(os.Args) != 4 {
+		fmt.Println("Usage: ./golangscript <threads> <output-file.txt> <btc-address-file.txt>")
+		os.Exit(1)
+	}
 
-    numThreads, err := strconv.Atoi(os.Args[1])
-    if err != nil {
-        log.Fatalf("Invalid number of threads: %s", err)
-    }
+	numThreads, err := strconv.Atoi(os.Args[1])
+	if err != nil {
+		log.Fatalf("Invalid number of threads: %s", err)
+	}
 
-    outputFile := os.Args[2]
-    btcAddressesFile := os.Args[3]
+	outputFile := os.Args[2]
+	btcAddressesFile := os.Args[3]
 
-    btcAddresses, err := readAddresses(btcAddressesFile)
-    if err != nil {
-        log.Fatalf("Failed to read BTC addresses: %s", err)
-    }
+	btcAddresses, err := readAddresses(btcAddressesFile)
+	if err != nil {
+		log.Fatalf("Failed to read BTC addresses: %s", err)
+	}
 
-    var wg sync.WaitGroup
-    var mutex sync.Mutex
+	var wg sync.WaitGroup
+	var mutex sync.Mutex
 
-    for i := 0; i < numThreads; i++ {
-        wg.Add(1)
-        go worker(i, &wg, &mutex, outputFile, btcAddresses)
-    }
+	for i := 0; i < numThreads; i++ {
+		wg.Add(1)
+		go worker(i, &wg, &mutex, outputFile, btcAddresses)
+	}
 
-    wg.Wait()
+	wg.Wait()
 }
--- go.mod
+++ go.mod
@@ -3,6 +3,14 @@ module btcgen
 go 1.21.5
 
 require (
-	github.com/btcsuite/btcutil v1.0.2
-	golang.org/x/crypto v0.17.0
+	golang.org/x/crypto v0.31.0
+)
+
+require (
+	github.com/btcsuite/btcd v0.24.2 // indirect
+	github.com/btcsuite/btcd/btcec/v2 v2.3.4 // indirect
+	github.com/btcsuite/btcd/btcutil v1.1.6 // indirect
+	github.com/btcsuite/btcd/chaincfg/chainhash v1.1.0 // indirect
+	github.com/decred/dcrd/dcrec/secp256k1/v4 v4.3.0 // indirect
+	golang.org/x/sys v0.28.0 // indirect
 )
