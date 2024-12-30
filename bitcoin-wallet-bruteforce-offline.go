f473fce19d0aea1f2977148edcd665551c310b9d
Alexandre loubeyres <alexandre.loubeyres@gmail.com>
2024-12-30 01:04:32 +0100

    full implementation

--- bitcoin-wallet-bruteforce-offline.go
+++ bitcoin-wallet-bruteforce-offline.go
@@ -5,9 +5,11 @@ package main
 import (
 	"bufio"
 	"encoding/hex"
+	"errors"
 	"fmt"
 	"github.com/btcsuite/btcd/btcec/v2"
 	"github.com/btcsuite/btcd/btcutil/base58"
+	"github.com/btcsuite/btcd/btcutil/bech32"
 	"log"
 	"os"
 	"strconv"
@@ -19,6 +21,104 @@ import (
 
 // Version byte for mainnet = 0x00
 const VERSION_BYTE = 0x00
+
+const (
+	P2PKH_VERSION = 0x00 // Mainnet P2PKH
+	P2SH_VERSION  = 0x05 // Mainnet P2SH
+	BECH32_HRP    = "bc" // Mainnet Bech32
+)
+
+type AddressType int
+
+const (
+	P2PKH AddressType = iota
+	P2SH_SEGWIT
+	NATIVE_SEGWIT
+)
+
+type BtcAddress struct {
+	p2pkh  string
+	p2sh   string
+	bech32 string
+}
+
+func createBitcoinAddress(pubKey []byte, addrType AddressType) (string, error) {
+	switch addrType {
+	case P2PKH:
+		return createP2PKHAddress(pubKey)
+	case P2SH_SEGWIT:
+		return createP2SHSegWitAddress(pubKey)
+	case NATIVE_SEGWIT:
+		return createNativeSegWitAddress(pubKey)
+	default:
+		return "", errors.New("invalid address type")
+	}
+}
+
+func createP2PKHAddress(pubKey []byte) (string, error) {
+	// SHA256 then RIPEMD160
+	hash := hash160(pubKey)
+
+	// Add version byte
+	versioned := append([]byte{P2PKH_VERSION}, hash...)
+
+	// Add checksum
+	checksum := doubleSHA256(versioned)[:4]
+	full := append(versioned, checksum...)
+
+	return base58.Encode(full), nil
+}
+
+func createP2SHSegWitAddress(pubKey []byte) (string, error) {
+	// SegWit program: version 0 + 20-byte pubkey hash
+	hash := hash160(pubKey)
+	program := append([]byte{0x00}, hash...)
+
+	// Script: OP_0 + push(program)
+	redeemScript := append([]byte{0x00, 0x14}, program...)
+
+	// P2SH: hash160(redeemScript)
+	scriptHash := hash160(redeemScript)
+
+	// Add P2SH version
+	versioned := append([]byte{P2SH_VERSION}, scriptHash...)
+	checksum := doubleSHA256(versioned)[:4]
+	full := append(versioned, checksum...)
+
+	return base58.Encode(full), nil
+}
+
+func createNativeSegWitAddress(pubKey []byte) (string, error) {
+	program := hash160(pubKey)
+
+	// Bech32 encoding with witness version 0
+	conv, err := bech32.ConvertBits(program, 8, 5, true)
+	if err != nil {
+		return "", err
+	}
+
+	combined := append([]byte{0x00}, conv...)
+	address, err := bech32.Encode(BECH32_HRP, combined)
+	if err != nil {
+		return "", err
+	}
+
+	return address, nil
+}
+
+// Helper functions
+func hash160(data []byte) []byte {
+	sha := sha256.Sum256(data)
+	ripe := ripemd160.New()
+	ripe.Write(sha[:])
+	return ripe.Sum(nil)
+}
+
+func doubleSHA256(data []byte) []byte {
+	hash1 := sha256.Sum256(data)
+	hash2 := sha256.Sum256(hash1[:])
+	return hash2[:]
+}
 
 func readAddresses(filePath string) (map[string]bool, error) {
 	addresses := make(map[string]bool)
@@ -41,52 +141,42 @@ func readAddresses(filePath string) (map[string]bool, error) {
 	return addresses, nil
 }
 
-func generateKeyAndAddress() (string, string, error) {
+func generateKeyAndAddress() (string, BtcAddress, error) {
 	privateKey, err := btcec.NewPrivateKey()
 
 	if err != nil {
-		return "", "", err
+		return "", BtcAddress{}, err
 	}
 
 	serializedPublicKey := privateKey.PubKey().SerializeCompressed()
-	address, err := publicKeyToAddress(serializedPublicKey)
-
+	bitcoinAddressP2PKH, err := createBitcoinAddress(serializedPublicKey, P2PKH)
 	if err != nil {
-		return "", "", err
+		return "", BtcAddress{}, err
 	}
-	return hex.EncodeToString(privateKey.Serialize()), address, nil
+	bitcoinAddressP2SH, err := createBitcoinAddress(serializedPublicKey, P2SH_SEGWIT)
+	if err != nil {
+		return "", BtcAddress{}, err
+	}
+	bitcoinAddressBech32, err := createBitcoinAddress(serializedPublicKey, NATIVE_SEGWIT)
+	if err != nil {
+		return "", BtcAddress{}, err
+	}
+
+	return hex.EncodeToString(privateKey.Serialize()), BtcAddress{bitcoinAddressP2PKH, bitcoinAddressP2SH, bitcoinAddressBech32}, nil
 }
 
-func publicKeyToAddress(pubKey []byte) (string, error) {
-
-	// 1. SHA256
-	sha256Hash := sha256.Sum256(pubKey)
-
-	// 2. RIPEMD160
-	ripemd160Hash := ripemd160.New()
-	_, err := ripemd160Hash.Write(sha256Hash[:])
-	if err != nil {
-		return "", err
+func btcAddressExist(address BtcAddress, btcAddresses map[string]bool) (string, bool) {
+	if btcAddresses[address.p2pkh] {
+		return address.p2pkh, true
 	}
-	ripemdHash := ripemd160Hash.Sum(nil)
+	if btcAddresses[address.p2sh] {
+		return address.p2sh, true
+	}
+	if btcAddresses[address.bech32] {
+		return address.bech32, true
+	}
 
-	// 3. Add version byte
-	versionedHash := append([]byte{VERSION_BYTE}, ripemdHash...)
-
-	// 4. Double SHA256 for checksum
-	firstSHA := sha256.Sum256(versionedHash)
-	secondSHA := sha256.Sum256(firstSHA[:])
-
-	// 5. First 4 bytes of double-SHA is checksum
-	checksum := secondSHA[:4]
-
-	// 6. Concat version + ripemdHash + checksum
-	finalHash := append(versionedHash, checksum...)
-
-	// 7. Base58 encode
-	address := base58.Encode(finalHash)
-
-	return address, nil
+	return "", false
 }
 
 func worker(id int, wg *sync.WaitGroup, mutex *sync.Mutex, outputFile string, btcAddresses map[string]bool) {
@@ -99,8 +189,8 @@ func worker(id int, wg *sync.WaitGroup, mutex *sync.Mutex, outputFile string, bt
 			continue
 		}
 
-		if _, exists := btcAddresses[publicAddress]; exists {
-			fmt.Printf("Match Found! Privatekey: %s Publicaddress: %s\n", privateKey, publicAddress)
+		if publicAddressKey, exists := btcAddressExist(publicAddress, btcAddresses); exists {
+			fmt.Printf("Match Found! Privatekey: %s Publicaddress: %s\n", privateKey, publicAddressKey)
 
 			mutex.Lock()
 			file, err := os.OpenFile(outputFile, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
@@ -110,7 +200,7 @@ func worker(id int, wg *sync.WaitGroup, mutex *sync.Mutex, outputFile string, bt
 				continue
 			}
 
-			if _, err := file.WriteString(fmt.Sprintf("%s:%s\n", privateKey, publicAddress)); err != nil {
+			if _, err := file.WriteString(fmt.Sprintf("%s:%s\n", privateKey, publicAddressKey)); err != nil {
 				log.Printf("Worker %d: Failed to write to file: %s", id, err)
 			}
 			file.Close()
